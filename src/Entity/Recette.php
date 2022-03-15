ca1ba1fce53a95ecfdb2e22de76cec0e3724f59e
ndiayemtr <ndiayemtr@gmail.com>
2022-03-15 10:59:05 +0000

    create entity Recette et constraints

---
+++ src/Entity/Recette.php
@@ -0,0 +1,215 @@
+<?php
+
+namespace App\Entity;
+
+use DateTimeImmutable;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\RecetteRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Validator\Constraints as Assert; 
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+#[UniqueEntity('name')]
+#[ORM\HasLifecycleCallbacks]
+#[ORM\Entity(repositoryClass: RecetteRepository::class)]
+class Recette
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    #[Assert\NotBlank()]
+    #[Assert\Length(min: 2, max: 50)]
+    private string $name;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    #[Assert\Positive()]
+    #[Assert\LessThan(1441)]
+    private ?int $time;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    #[Assert\Positive()]
+    #[Assert\LessThan(50)]
+    private ?int $nbPersonne;
+
+    #[ORM\Column(type: 'text')]
+    #[Assert\NotBlank()]
+    private string   $description;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    #[Assert\Positive()]
+    #[Assert\LessThan(6)]
+    private ?int $difficulte;
+
+    #[ORM\Column(type: 'float', nullable: true)]
+    #[Assert\Positive()]
+    #[Assert\LessThan(1001)]
+    private ?float $price;
+
+    #[ORM\Column(type: 'boolean')]
+    private bool $isFavorite;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    #[Assert\NotBlank()]
+    private \DateTimeImmutable $createdAt;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    #[Assert\NotBlank()]
+    private \DateTimeImmutable $updatedAt;
+
+    #[ORM\ManyToMany(targetEntity: ingredient::class)]
+    private $ingredients;
+
+    public function __construct()
+    {
+        $this->ingredients = new ArrayCollection();
+        $this->createdAt = new DateTimeImmutable();
+        $this->updatedAt = new DateTimeImmutable();
+    }
+
+    #[ORM\PrePersist()]
+    public function setUpdateAttValue(): void
+    {
+        $this->updatedAt = new DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getTime(): ?int
+    {
+        return $this->time;
+    }
+
+    public function setTime(?int $time): self
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    public function getNbPersonne(): ?int
+    {
+        return $this->nbPersonne;
+    }
+
+    public function setNbPersonne(?int $nbPersonne): self
+    {
+        $this->nbPersonne = $nbPersonne;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDifficulte(): ?int
+    {
+        return $this->difficulte;
+    }
+
+    public function setDifficulte(?int $difficulte): self
+    {
+        $this->difficulte = $difficulte;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getIsFavorite(): ?bool
+    {
+        return $this->isFavorite;
+    }
+
+    public function setIsFavorite(bool $isFavorite): self
+    {
+        $this->isFavorite = $isFavorite;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, ingredient>
+     */
+    public function getIngredients(): Collection
+    {
+        return $this->ingredients;
+    }
+
+    public function addIngredient(ingredient $ingredient): self
+    {
+        if (!$this->ingredients->contains($ingredient)) {
+            $this->ingredients[] = $ingredient;
+        }
+
+        return $this;
+    }
+
+    public function removeIngredient(ingredient $ingredient): self
+    {
+        $this->ingredients->removeElement($ingredient);
+
+        return $this;
+    }
+}
