69b84f3f77ae8024d06a2f8ef04ed7170ed19b6a
ndiayemtr <ndiayemtr@gmail.com>
2022-03-17 15:24:55 +0000

    creta user & password hasher listener

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,12 +3,14 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
-use Faker\Generator;
+use App\Entity\User;
 
+use Faker\Generator;
 use App\Entity\Recette;
 use App\Entity\Ingredient;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
@@ -17,13 +19,16 @@ class AppFixtures extends Fixture
      */
     private Generator $faker; 
 
-    public function __construct(){
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher){
         $this->faker = Factory::create('fr_FR');
+        $this->passwordHasher = $passwordHasher;
     }
 
     public function load(ObjectManager $manager): void
     {
-        for ($i=1; $i <= 50; $i++) { 
+      /*   for ($i=1; $i <= 50; $i++) { 
             $ingredient = new Ingredient();
              $ingredient->setName($this->faker->word())
                    ->setPrice(\mt_rand(0, 100));
@@ -48,6 +53,19 @@ class AppFixtures extends Fixture
 
             $manager->persist($recette);
                     
+        } */
+
+        for ($i=0; $i < 10; $i++) { 
+            $user = new User();
+           
+            $user->setFullName($this->faker->word())
+                ->setPseudo(mt_rand(0, 1) === 1 ? $this->faker->firstName() :  null)
+                ->setEmail($this->faker->email())
+                ->setRoles(['ROLE_USER'])
+                ->setPlainPassword('password');
+
+                $manager->persist($user);
+
         }
 
         
---
+++ src/EntityListener/UserListener.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\EntityListenner;
+
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserListener 
+{
+    private UserPasswordHasherInterface $passwordHasher;
+    public function __construct(UserPasswordHasherInterface $passwordHasher){
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function prePersist(User $user){
+        $this->encodePassword($user);
+
+    }
+
+    public function preUpdate(User $user){
+        $this->encodePassword($user);
+        
+    }
+
+    public function encodePassword(User $user){
+        if ($user->getPlainPassword() === null) {
+            return;
+        }
+        $user->setPassword(
+            $this->passwordHasher->hashPassword(
+                $user,
+                $user->getPlainPassword()
+            )
+            );
+         
+    }
+    
+}
